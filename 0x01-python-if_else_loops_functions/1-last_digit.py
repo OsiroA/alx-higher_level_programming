8422dfab95662b3d45c8b42dbd3dbb9c34639d0f
OsiroA <osiroasunde@gmail.com>
2023-05-09 15:02:35 +0100

    handled the caswe for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = number % 10
-if digit > 5:
-    print(f"Last digit of {number} is {digit} and is greater than 5")
-if digit == 0:
-    print(f"Last digit of {number} is {digit} and is 0")
-if digit < 6 and digit != 0:
-    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
+if number < 0:
+    number = abs(number)
+    digit = -(number % 10)
+    digit = number % 10
+    if digit > 5:
+        print(f"Last digit of {number} is {digit} and is greater than 5")
+    if digit == 0:
+        print(f"Last digit of {number} is {digit} and is 0")
+    if digit < 6 and digit != 0:
+        print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
+else:
+    digit = number % 10
+    if digit > 5:
+        print(f"Last digit of {number} is {digit} and is greater than 5")
+    if digit == 0:
+        print(f"Last digit of {number} is {digit} and is 0")
+    if digit < 6 and digit != 0:
+        print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
