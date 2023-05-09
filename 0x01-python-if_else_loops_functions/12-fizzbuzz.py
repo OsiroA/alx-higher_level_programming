006dcdb477dbef285213c91437dc4f43895d8b50
OsiroA <osiroasunde@gmail.com>
2023-05-09 16:29:24 +0100

    I corrected the spacing issue in the fizzbuzz code

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for number in range(1, 101):
         if number % 15 == 0:
-            print("FizzBuzz ", end=" ")
+            print("FizzBuzz", end=" ")
         elif number % 5 == 0:
             print("Buzz", end=" ")
         elif number % 3 == 0:
