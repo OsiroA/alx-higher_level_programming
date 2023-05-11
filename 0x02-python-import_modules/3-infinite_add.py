6ca449d5d31d8a0733ee56d45377b7f2ca1a7dab
OsiroA <osiroasunde@gmail.com>
2023-05-11 15:27:58 +0100

    Added a try and except block for when there is an invalid argument

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,10 +5,15 @@ if __name__ == "__main__":
     # get the length of the arguments passed on the command line
     c = sys.argv
     x = len(c) - 1
-    a, b = 0, int(sys.argv[1])
-    for i in range(2, (x + 1)):
-        a, b = b, b + int(sys.argv[i])
+    a, b = 0, 0
+    #a, b = 0, int(sys.argv[1])
+    for i in range(1, (x + 1)):
+        try:
+            b = int(sys.argv[i])
+            a += b
+        except:
         # i += 1
-        if i == x:
-            a = a + int(sys.argv[i])
+        #if i == x:
+            print("Invalid argumment: {}".format(sys.argv[i]))
+            sys.exit(1)
     print(a)
