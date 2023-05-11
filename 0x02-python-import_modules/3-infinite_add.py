fd66125d7c6141ebf409822ff83963053fc1b9c2
OsiroA <osiroasunde@gmail.com>
2023-05-11 15:31:13 +0100

    corrected linter errors according to pycodestyle

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     c = sys.argv
     x = len(c) - 1
     a, b = 0, 0
-    #a, b = 0, int(sys.argv[1])
+    # a, b = 0, int(sys.argv[1])
     for i in range(1, (x + 1)):
         try:
             b = int(sys.argv[i])
             a += b
-        except:
-        # i += 1
-        #if i == x:
+        except ValueError:
+            # i += 1
+            # if i == x:
             print("Invalid argumment: {}".format(sys.argv[i]))
             sys.exit(1)
     print(a)
