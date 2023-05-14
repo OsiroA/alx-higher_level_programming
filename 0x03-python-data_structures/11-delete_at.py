346b50105344c7f5ea21975a9cfcb3ae194b723a
OsiroA <osiroasunde@gmail.com>
2023-05-14 12:43:19 +0100

    corrected my return statements for error handling

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx < 0:
-        return
-    elif idx > (len(my_list) - 1):
-        return
+        return my_list
+    elif idx not in range((len(my_list) - 1)):
+        return my_list
     else:
         my_list.remove(my_list[idx])
     return my_list
