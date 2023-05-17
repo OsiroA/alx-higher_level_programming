ada0ec3488c967b1a61178e9e6c56b494f94d198
OsiroA <osiroasunde@gmail.com>
2023-05-17 15:31:05 +0100

    Removed all conditiional statements as they are redundant

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for k, v in a_dictionary.items():
-        if key in a_dictionary.keys() and value in a_dictionary.values():
-            a_dictionary[key] = value
-        elif key in a_dictionary.keys() and value not in a_dictionary.values():
-            a_dictionary[key] = value
-        else:
-            a_dictionary[key] = value
-        return (a_dictionary)
+    a_dictionary[key] = value
+    return (a_dictionary)
