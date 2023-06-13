967e8da830ead3f52d14b15f910331f7c9235140
OsiroA <osiroasunde@gmail.com>
2023-06-13 11:40:13 -0700

    Create 1-write_file This file contains a function that writes a string to a text file and returns the number of characters written

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def write_file(filename="", text=""):
+    """This writes to a file"""
+    noOfChars = 0
+    with open(filename, "w", encoding="utf-8") as afile:
+        afile.write(text)
+    return len(text)
