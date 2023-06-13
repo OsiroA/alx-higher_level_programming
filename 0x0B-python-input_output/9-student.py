b85d0781863f0345a9b5cfa4df6d36896078fbe9
OsiroA <osiroasunde@gmail.com>
2023-06-13 14:35:58 -0700

    I nmade pycodestyle correcxtions

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,10 +3,11 @@
 
 
 class Student:
-  """The class student"""
-  def __init__(self, first_name, last_name, age):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-  def to_json(self):
-    return self.__dict__
+    """The class student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
