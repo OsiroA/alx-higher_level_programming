1f813487782723ec2ee6c1f185f623a6e477a0da
OsiroA <osiroasunde@gmail.com>
2023-06-12 17:09:37 -0700

    Update 8-rectangel I imported the BaseGeometry instead of adding it to the stat if my code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,21 +3,7 @@
 """
 
 
-class BaseGeometry:
-    """an BaseGeometry class
-    """
-    def area(self):
-        """This is supposed to define an area of a geometry
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """This validates a string called Value
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
