7efab337e2b15df54e07d7208541685c5ffc844b
OsiroA <osiroasunde@gmail.com>
2023-06-13 15:08:41 -0700

    rewroter the second part of the code

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,22 +6,15 @@ def pascal_triangle(n):
     """this function returns a list kf lists of integers
     representing the triangle
     """
-    list1 = []
     if n <= 0:
-        return list1
-    for numb in range(0, n):
-        if numb == 0:
-            list1.append([1])
-        else:
-            list2 = []
-            len_last = len(list1[-1])
-            index = len(list1) - 1
-            for numb2 in range(0, len_last):
-                if numb2 == 0:
-                    list2.append(1)
-                if numb2 == len_last - 1:
-                    list2.append(1)
-                else:
-                    list2.append(list1[idx][numb2] + list1[idx][numb2 + 1])
-            list1.append(list2)
-    return list1
+        return []
+
+    triad = [[1]]
+    while len(triad) != n:
+        meta = triad[-1]
+        unp = [1]
+        for index in range(len(meta) - 1):
+            unp.append(meta[index] + meta[index + 1])
+        unp.append(1)
+        triad.append(unp)
+    return triad
