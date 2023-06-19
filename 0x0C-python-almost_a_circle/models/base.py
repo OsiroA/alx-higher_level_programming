dc34643aa2e12a8bd0de9d8cbf9baa3f3ee11f74
OsiroA <osiroasunde@gmail.com>
2023-06-19 10:14:29 -0700

    DOne with the mandatory tasks, aboiut to run the actual checker

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """this is for the base file"""
+import json
+import csv
+import turtle
 
 
 class Base:
@@ -13,3 +16,52 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """this returns the JSON string representaion opf the given input"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """this is a class method that writes the JSON string representation
+        of a given list to a file"""
+        afile = cls.__name__ + ".json"
+        with open(afile, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                listDict = [b.to_dictionary() for b in list_objs]
+                jsonfile.write(Base.to_json_string(listDict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """This static method retutns the list
+        represented in JSON format"""
+        if json_string is None or json_string == "[]":
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                newClass = cls(1, 1)
+            else:
+                newClass = cls(1)
+        newClass.update(**dictionary)
+        return newClass
+
+    @classmethod
+    def load_from_file(cls):
+        """This returns a list of instances"""
+        afile = str(cls.__name__) + ".json"
+        try:
+            with open(afile, "r") as jsonfile:
+                listDict = Base.from_json_string(jsonfile.read())
+                return [cls.create(**b) for b in listDict]
+        except IOError:
+            return []
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -111,3 +111,13 @@ class Rectangle(Base):
             self.__y = args[4]
         except IndexError:
             pass
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a rectangle"""
+        return {
+                "id": self.id,
+                "width": self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+                }
