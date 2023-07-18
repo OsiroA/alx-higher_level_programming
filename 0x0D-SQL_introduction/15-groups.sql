02e9121761e3e6c79db97e53919c1e7fb36bc987
OsiroA <osiroasunde@gmail.com>
2023-07-18 07:59:19 -0700

    syntax error

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
--- this script lists the number of records with the same score in the table
-SELECT score, COUNT(score) AS number GROUP BY score ORDER BY number DESC;
+-- this script lists the number of records with the same score in the tablE
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
