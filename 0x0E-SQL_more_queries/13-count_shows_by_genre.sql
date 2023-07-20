e8b872ae8ddd21d7e5273fddf90a3a973ae970e6
OsiroA <osiroasunde@gmail.com>
2023-07-20 12:04:50 -0700

    syntax errors on t13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- This script lists all genres from the database and displays the number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+SELECT g.name AS genre, COUNT(s.genre_id) AS number_of_shows
 FROM tv_genres AS g
 INNER JOIN tv_show_genres AS s
 ON g.id = s.genre_id
-GROUP BY g.name
+GROUP BY genre
 ORDER BY number_of_shows DESC;
