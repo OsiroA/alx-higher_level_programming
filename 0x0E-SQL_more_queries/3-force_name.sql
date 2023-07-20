261f6e415eb846c670248302466998e1a53c6991
OsiroA <osiroasunde@gmail.com>
2023-07-20 10:19:47 -0700

    task 3 correction agaion

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,4 +2,4 @@
 -- The database name will be passed as an argument on the mysql command
 CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
-	name VARCHAR(256), NOT NULL);
+	name VARCHAR(256) NOT NULL);
