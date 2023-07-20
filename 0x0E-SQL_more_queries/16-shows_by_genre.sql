26a6d00a8e8c12a348e1396f85f423c3084fb7e0
OsiroA <osiroasunde@gmail.com>
2023-07-20 13:56:31 -0700

    ;corrected a keyword case

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT t.title, g.name
 FROM tv_shows AS t
 LEFT JOIN tv_show_genres AS s
 ON t.id = s.show_id
-LEFT JOIN tv_genres as g
+LEFT JOIN tv_genres AS g
 ON s.genre_id = g.id
 ORDER BY t.title ASC, g.name ASC;
