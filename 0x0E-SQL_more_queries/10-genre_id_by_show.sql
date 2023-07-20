f9f5e51609c1423118342737b13b2c77c1553bc3
OsiroA <osiroasunde@gmail.com>
2023-07-20 10:59:31 -0700

    task 10 added the missing semi-column at EOL

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 -- This script then lists all shows contsined the the dump that have atleast one genre linekd
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title AND tv_show_genres.genre_id
+	INNER JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
