b95dedf145f6299c65397aa41b6389f8a7aa2605
OsiroA <osiroasunde@gmail.com>
2023-07-20 10:34:46 -0700

    task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- This script lists all the cities of california that can e found in the database
 SELECT id, name
 FROM cities, states
-WHERE state_id IN (
+WHERE state_id = (
 	SELECT id
 	FROM states
 	WHERE name = 'California')
