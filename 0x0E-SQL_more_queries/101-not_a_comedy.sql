ef59bd1747135b3b4c794f4bb22e5cbb70e200cf
OsiroA <osiroasunde@gmail.com>
2023-07-20 14:29:02 -0700

    task 17

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -4,6 +4,8 @@ FROM tv_shows AS s
 WHERE s.id NOT IN (
 	SELECT tv_shows.id
 	FROM tv_shows
+	INNER JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
 	INNER JOIN tv_genres AS g
 	ON tv_show_genres.genre_id = g.id
 	WHERE g.name = "Comedy")
