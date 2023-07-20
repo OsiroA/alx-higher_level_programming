64be3c06abb06d31df3e7bd6b0f34117672c7b11
OsiroA <osiroasunde@gmail.com>
2023-07-20 11:36:18 -0700

    task 11 corrected syntax errors

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- This script lists all shows contained in a specified database on the command lline
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows as t
-LEFT JOIN tv_show_genre as g
-ON t.id = g.show_id
+SELECT t.title, g.genre_id
+FROM tv_shows AS t
+	LEFT JOIN tv_show_genre AS g
+	ON t.id = g.show_id
 ORDER BY t.title, g.genre_id ASC;
