8998eec0b87c2d43241c6cef1407a069e60e8aa8
OsiroA <osiroasunde@gmail.com>
2023-08-15 05:10:45 -0700

    corrected a typo in variable name

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* This class defines a Rectangle with a constructor */
-module.exports = class rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
