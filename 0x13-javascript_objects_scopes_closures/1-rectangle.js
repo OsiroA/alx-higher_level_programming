c4e12bc99d9c44ae5dd1c9522bdfda15b220f13e
OsiroA <osiroasunde@gmail.com>
2023-08-15 05:09:07 -0700

    this script defiunes a rectangle with width and height

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* This class defines a Rectangle with a constructor */
+module.exports = class rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
