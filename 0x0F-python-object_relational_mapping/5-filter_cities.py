0b76c16b3303d5b9e5b3338cc7ccfb056660644b
OsiroA <osiroasunde@gmail.com>
2023-08-19 14:59:45 -0700

    This script contains the class Definition of a State and an instance base

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,18 +7,27 @@ This script lists all cities in a specified state a given database
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
+    contd = 0
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
                  FROM cities \
-                 INNER JOIN states ON cities.state_id = states.id \
+                 LEFT JOIN states ON cities.state_id = states.id \
                  ORDER BY cities.id ASC")
     rows = cur.fetchall()
 
+    for row in rows:
+        if row[2] == argv[4]:
+            if contd > 0:
+                print(", ", end="")
+            print(row[1], end="")
+            contd = contd + 1
+    print()
+    # print(*[row[0] for row in rows], sep=", ")
     # for row in rows:
         # print(row)
-    if rows is not None:
-        print(", ".join([row[2] if row[4] == argv[4]]))
+    # if rows is not None:
+      #  print(", ".join([row[2] if row[4] == argv[4]]))
     cur.close()
     db.close()
