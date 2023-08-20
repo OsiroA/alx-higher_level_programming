e7807f0a12019b085701a447a3caee1108021d0a
OsiroA <osiroasunde@gmail.com>
2023-08-20 15:08:14 -0700

    These scripts define Class City, and this prints all objects

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''
-This script changes the name of the state object
-where id=2 to New Mexico to a given database
+This script lists all state objects from a given database
 '''
 
 
@@ -9,22 +8,21 @@ from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from model_city import City
 
 if __name__ == "__main__":
     '''
     This script gets the states from thr database
     '''
     database_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            argv[1], argv[2], argv[3], pool_pre_ping=True)
+            argv[1], argv[2], argv[3])
     database_engine = create_engine(database_uri)
-
     Session = sessionmaker(bind=database_engine)
 
     session = Session()
-    # state = session.query(State).filter_by(id=2).first()
-    for state in session.query(State):
-        if "a" in state.name:
-            session.delete(state)
-    session.commit()
-    # print(newObject.id)
+
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This scripts contains the class definition of a City
+"""
+
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    '''
+    Class City
+    '''
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
