8231236038396d0e476d72b61ec97f8c90c496ad
OsiroA <osiroasunde@gmail.com>
2023-08-20 15:49:44 -0700

    These scripts improve existing files and create a relationship, a new City and a new State

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -8,22 +8,24 @@ where id=2 to New Mexico to a given database
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+# from model_state import Base, State
+from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == "__main__":
     '''
     This script gets the states from thr database
     '''
     database_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            argv[1], argv[2], argv[3], pool_pre_ping=True)
+            argv[1], argv[2], argv[3])
     database_engine = create_engine(database_uri)
-
     Session = sessionmaker(bind=database_engine)
 
     session = Session()
     # state = session.query(State).filter_by(id=2).first()
-    for state in session.query(State).filter(State.name.contains('a')):
-            session.delete(state)
+    newState = State(name='California')
+    newCity = City(name='San Francisco')
+    newState.cities.append(new_city)
+    session.add(newState)
     session.commit()
-    # print(newObject.id)
     session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+This is a script thst contains a class definition
+'''
+import sys
+# from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''
+    This inherits from Base and links to the MySQL tsble
+    '''
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref='state')
