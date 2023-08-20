6ab942002898d010000fe2f78e831e68b14f91e9
OsiroA <osiroasunde@gmail.com>
2023-08-20 13:28:46 -0700

    This script changes the name of a stste in a database

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+This script changes the name of the state object
+where id=2 to New Mexico to a given database
+'''
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    '''
+    This script gets the states from thr database
+    '''
+    database_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1], argv[2], argv[3])
+    database_engine = create_engine(database_uri)
+    Session = sessionmaker(bind=database_engine)
+
+    session = Session()
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    session.commit()
+    # print(newObject.id)
+    session.close()
