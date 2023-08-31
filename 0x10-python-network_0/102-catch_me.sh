559b1b1dea37dc9ca90798ecb20a921c1c28be5c
OsiroA <124213013+OsiroA@users.noreply.github.com>
2023-08-31 23:29:07 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This makes a request to an address that caused the server tk respkmd with a custom
-curl -s -X PUT "http://0.0.0.0:5000/catch_me" -d "user_id=98" -L | grep "You got me!"
+curl -s -X PUT "http://0.0.0.0:5000/catch_me" | grep "You got me!"
