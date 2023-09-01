6c14c454a3df1e9b4413e7ac27764155c19ba95c
OsiroA <osiroasunde@gmail.com>
2023-09-01 06:56:46 -0700

    added a with statement though unsure of my tabulation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     # url to fetch is:
     url = 'https://alx-intranet.hbtn.io/status'
     # then send an HTTP GET request to the URL
-    response = urllib.request.urlopen(url)
-    # read the HTML content
-    htmlContent = response.read()
-    # print the content in the specified format by school
-    print("Body response:")
-    print("\t- type: {}".format(type(htmlContent)))
-    print("\t- content: {}".format(htmlContent))
-    print("\t- utf8 content: {}".format(htmlContent.decode('utf-8')))
+    with urllib.request.urlopen(url) as response:
+        # read the HTML content
+        htmlContent = response.read()
+        # print the content in the specified format by school
+        print("Body response:")
+        print("\t- type: {}".format(type(htmlContent)))
+        print("\t- content: {}".format(htmlContent))
+        print("\t- utf8 content: {}".format(htmlContent.decode('utf-8')))
