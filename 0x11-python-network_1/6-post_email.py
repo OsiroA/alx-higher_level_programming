16fe50c6d55329512d07e546fb9ec909eaa59252
OsiroA <osiroasunde@gmail.com>
2023-09-01 09:28:43 -0700

    this script posts an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL and an email address
+sends a POST request to the passed URL with the email as a parameter
+and then displays the body of the response
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = {'email': sys.argv[2]}
+    response = requests.post(url, data)
+    print(response.text)
