ea6187fab6c936d270580cc4f63287f95680cef0
OsiroA <osiroasunde@gmail.com>
2023-09-01 06:43:53 -0700

    this file fetches a website

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+this script fetches a given website using urrlib
+"""
+
+
+import urllib.request
+# url to fetch is:
+url = 'https://alx-intranet.hbtn.io/status'
+# then send an HTTP GET request to the URL
+response = urllib.request.urlopen(url)
+# read the HTML content
+htmlContent = response.read()
+# print the content in the specified format by school
+print("Body response:")
+print("\t- type: {}".format(type(htmlContent)))
+print("\t- content: {}".format(htmlContent))
+print("\t- utf8 content: {}".format(htmlContent.decode('utf-8')))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks on urllib requests in Python organized bu ALX and Holberton
